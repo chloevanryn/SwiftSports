ae2b7b145020d6587bf2ec9582a94c6317bf5882
Alan Glazer <alancglazer@gmail.com>
2023-01-03 04:57:09 -0600

    added some front end changes to landing page

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,9 @@
 class EventsController < ApplicationController
+  def index
+    @sporting_events = SportingEvent.all
+  end
+
+  def show
+    @sporting_event = SportingEvent.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   root to: "pages#home"
 
   resources :users, only: [:index, :show]
+  resources :events, only: [:index, :show]
 end
