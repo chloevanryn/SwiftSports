c6dc1e7b39009dc5fd8456d78892a437cf20a877
Arthur Shendrik <arti.cloud@me.com>
2023-01-04 17:06:19 +0100

    added groups form, view and index

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
 
-  get "/random", to: "groups#random"
-
-  # resources :groups, only: [:new, :create, :show]
+  resources :groups, only: [:new, :create, :show, :index]
   resources :users
 end
